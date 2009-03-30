c08f0cce1fc4006dc76dd0412ed56233b4b27055
Ben Schwarz <ben.schwarz@gmail.com>
2009-03-30 19:03:03 +1100

    Added source.rb class Added options to spec.opts

---
+++ lib/smoke/source/source.rb
@@ -0,0 +1,25 @@
+module Smoke
+  module Source
+    class Source
+    
+      attr_reader :items
+    
+      # Transform items in some way
+      # Not sure how to do this
+      def transform(collection, &block)
+      end
+    
+      # Rename properties in each item
+      def rename
+      end
+    
+      # Permit or restrict items based on filter results
+      def filter
+      end
+    
+      # Re-sort items
+      def sort
+      end
+    end
+  end
+end
