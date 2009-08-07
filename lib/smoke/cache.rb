5161a14d21d65874e3afca5de56727284dd87e53
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-08 00:14:02 +1000

    Specced configuration and stamped out some high level issues. (More to come)

--- lib/smoke/cache.rb
+++ lib/smoke/cache.rb
@@ -4,14 +4,15 @@ require 'digest/md5'
 module Smoke
   class Cache
     class << self
-      def configure
-        require "moneta/#{Smoke.config[:cache][:store].to_s.downcase}"
-        @@store = Moneta.const_get(Smoke.config[:cache][:store]).send(:new, Smoke.config[:cache][:options])
+      def configure!
+        require "moneta/#{Smoke.config[:cache][:store].to_s.downcase}" if enabled?
+      rescue LoadError
+        Smoke.log.fatal "Cache store not found. Smoke uses moneta for cache stores, ensure that you've chosen a moneta supported store"
       end
   
       def fetch(name, &block)
         key = generate_key(name)
-        output = (enabled?) ? @@store[key] : block.call
+        output = (enabled?) ? cache[key] : block.call
         if output
           return output
         else
@@ -26,9 +27,13 @@ module Smoke
         Smoke.config[:cache][:enabled]
       end
   
-      private 
+      private
+      def cache
+        Moneta.const_get(Smoke.config[:cache][:store]).send(:new, Smoke.config[:cache][:options]) 
+      end
+        
       def persist(key, store)
-        @@store.store(key, store, :expire_in => Smoke.config[:config][:cache][:expiry])
+        cache.store(key, store, :expire_in => Smoke.config[:config][:cache][:expiry])
       end
   
       def generate_key(key)
---
+++ spec/smoke/cache_spec.rb
@@ -0,0 +1,47 @@
+require File.join(File.dirname(__FILE__), "..", "spec_helper.rb")
+
+describe Smoke::Cache do
+  describe "class methods" do
+    it "should respond to configure!" do
+      Smoke::Cache.should respond_to(:configure!)
+    end
+    
+    it "should respond to fetch" do
+      Smoke::Cache.should respond_to(:fetch)
+    end
+    
+    it "should responsd to enabled?" do
+      Smoke::Cache.should respond_to(:enabled?)
+    end
+  end
+  
+  describe "configuration" do
+    it "should not be enabled (by default)" do
+      Smoke::Cache.should_not be_enabled
+    end
+    
+    it "should be enabled" do
+      Smoke.configure {|c| c[:cache][:enabled] = true }
+      Smoke::Cache.should be_enabled
+    end
+    
+    it "should not be enabled" do
+      Smoke.configure {|c| c[:cache][:enabled] = false }
+      Smoke::Cache.should_not be_enabled
+    end
+    
+    describe "invalid configuration" do
+      before do
+        @kernel = mock(Kernel)
+        @kernel.stub!(:exit)
+      end
+      
+      it "should log with bad configuration" do
+        Proc.new {
+          Smoke.should_receive(:log)
+          Smoke.configure {|c| c[:cache][:store] = :ponies }
+        }
+      end
+    end
+  end
+end
