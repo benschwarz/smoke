0fa53896198a83548acdc0fcb6453bec32b67314
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-07 20:30:13 +1000

    Working up caching implementation

--- lib/smoke/cache.rb
+++ lib/smoke/cache.rb
@@ -1,32 +1,39 @@
 require 'moneta'
 require 'digest/md5'
 
-module Smoke  
-  class << self
-    def configure
-      @@store = Moneta.const_get(Smoke.config[:cache][:store]).send(:new, Smoke.config[:cache][:options])
-    end
-    
-    def fetch(name, &block)
-      key = generate_key(name)
-      output = @@store[key]
-      if output
-        return output
-      else
-        output = block.call
-        persist(key, output)
+module Smoke
+  class Cache
+    class << self
+      def configure
+        require "moneta/#{Smoke.config[:cache][:store].to_s.downcase}"
+        @@store = Moneta.const_get(Smoke.config[:cache][:store]).send(:new, Smoke.config[:cache][:options])
       end
-      
-      return output
-    end
+  
+      def fetch(name, &block)
+        key = generate_key(name)
+        output = (enabled?) ? @@store[key] : block.call
+        if output
+          return output
+        else
+          output = block.call
+          persist(key, output)
+        end
     
-    private 
-    def persist(key, store)
-      @@store.store(key, store, :expire_in => Smoke.config[:config][:cache][:expiry])
-    end
-    
-    def generate_key(key)
-      Digest::MD5.hexdigest(key)
+        return output
+      end
+  
+      def enabled?
+        Smoke.config[:cache][:enabled]
+      end
+  
+      private 
+      def persist(key, store)
+        @@store.store(key, store, :expire_in => Smoke.config[:config][:cache][:expiry])
+      end
+  
+      def generate_key(key)
+        Digest::MD5.hexdigest(key)
+      end
     end
   end
 end
