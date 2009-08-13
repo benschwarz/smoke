bc3cffedf8d78d7eb5fb3c5f20868364305fb10e
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-13 23:31:59 +1000

    Added cache to examples

--- examples/web_search_joined.rb
+++ examples/web_search_joined.rb
@@ -1,3 +1,9 @@
+require 'lib/smoke'
+Smoke.configure do |c|
+  c[:enable_logging] = true
+  c[:cache][:enabled] = true
+  c[:cache][:store] = :memory
+end
 Smoke.yql(:python) do
   select :all
   from "search.web"
@@ -5,6 +11,8 @@ Smoke.yql(:python) do
   
   path :query, :results, :result
 end
+Smoke[:python].output
+
 
 Smoke.yql(:ruby) do
   select :all
