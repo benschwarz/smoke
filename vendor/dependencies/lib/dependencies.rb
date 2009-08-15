e524f4c86aba3e1a16948e8ba19a34dd7bef39f6
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-15 23:24:16 +1000

    One last try

--- vendor/dependencies/lib/dependencies.rb
+++ vendor/dependencies/lib/dependencies.rb
@@ -2,4 +2,4 @@ $:.unshift(File.expand_path(File.dirname(__FILE__)))
 
 require "dependencies/dep"
 
-Dep.new(File.read(File.join(File.dirname(__FILE__), '..', '..', '.dependencies'))).require(ENV["RACK_ENV"].to_s)
+Dep.new(File.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'dependencies'))).require(ENV["RACK_ENV"].to_s)
