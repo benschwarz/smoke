34e3261860a60c21d76c445237e9a30d45b72f63
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-24 19:40:50 +1000

    We're not doing dependencies like this anymore.

--- vendor/dependencies/lib/dependencies.rb
+++
@@ -1,5 +0,0 @@
-$:.unshift(File.expand_path(File.dirname(__FILE__)))
-
-require "dependencies/dep"
-
-Dep.new(File.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'dependencies'))).require(ENV["RACK_ENV"].to_s)
