3ab31b47591589e56d982b3f54a2c58ffa7ca58b
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-13 00:28:19 +1100

    Added support for parsing yaml - why not? (:

--- lib/smoke/transformers/yaml.rb
+++ lib/smoke/transformers/yaml.rb
@@ -2,9 +2,13 @@ module Smoke
   module Transformers
     class Yaml < Transformer
       identifier :yml, :yaml
-    
+      
       def self.generate(tree_name, objects)
         YAML.dump(objects)
+      end
+      
+      def self.parse(string)
+        YAML.load(string)
       end
     end
   end
